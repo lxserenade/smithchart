2fca24754e0f10aad4998f0767f03ebc04cd62d5
Andrew Lee <smiley325squared@gmail.com>
2011-05-09 23:10:10 -0500

    first commit

---
+++ smithchart.js
@@ -0,0 +1,15 @@
+function SmithChart (dimension)
+{
+    this.width = dimension;
+    this.height = dimension;
+}
+
+SmithChart.prototype.drawOuterRadialAxis = function (axisWidth)
+{
+    // Draw the outer axis (wavelength scale)
+};
+
+SmithChart.prototype.drawInnerRadialAxis = function (axisWidth)
+{
+    // Draw the inner axis (degree/angle scale)
+};
